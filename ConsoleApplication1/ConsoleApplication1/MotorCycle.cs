ec755a83743839f44b5fe2dc2380c8b6d30f3037
Vlad Tsvang <vtsvang@gmail.com>
2015-08-26 19:53:02 -0700

    Added ConsoleApplication1

---
+++ ConsoleApplication1/ConsoleApplication1/MotorCycle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorcycle
+{
+    class MotorCycle
+    {
+        public string Make;
+        public string Color;
+        public bool EngineState;
+
+        public void StartEngine() 
+        {
+            if (EngineState == true)
+            {
+                System.Console.WriteLine("The engine is already on.");
+            }
+            else
+            {
+                EngineState = true;
+                System.Console.WriteLine("The engine is now on");
+            }
+        }
+
+        public void ShowAtts()
+        {
+            System.Console.WriteLine("This motorcycle is a "
+                + Color + " " + Make);
+
+            if (EngineState == true)
+            {
+                System.Console.WriteLine("The engine is on");
+            }
+            else
+            {
+                System.Console.WriteLine("The engine is off");
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/MotorcycleTester.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Motorcycle;
+
+namespace MotorcycleTester
+{
+    class MotorcycleTester
+    {
+        public static void Main(string[] args)
+        {
+            MotorCycle m = new MotorCycle();
+            m.Make = "Yamaha RZ350";
+            m.Color = "yellow";
+            System.Console.WriteLine("Calling ShowAtts...");
+            m.ShowAtts();
+            System.Console.WriteLine("________");
+            System.Console.WriteLine("Starting engine...");
+            m.StartEngine();
+            System.Console.WriteLine("________");
+            System.Console.WriteLine("Calling ShowAtts...");
+            m.ShowAtts();
+            System.Console.WriteLine("________");
+            System.Console.WriteLine("Starting engine...");
+            m.StartEngine();
+        }
+    }
+}
